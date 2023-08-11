acf152c78845f1c5ca7c0afe235d7e1fbf091de6
Liempo <gonzalesalec@gmail.com>
2023-08-12 02:57:35 +0800

    Initial template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<main class="h-screen w-full bg-red-300">
+	<div class="flex flex-col items-center justify-center h-full">
+		<h1 class="text-4xl font-bold text-slate-900">Cyberdeck</h1>
+	</div>
+</main>
