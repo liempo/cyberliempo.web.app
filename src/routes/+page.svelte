c96122963b324a3bb8d08d0fd0d7d072822fa9f8
Liempo <gonzalesalec@gmail.com>
2023-08-12 13:54:38 +0800

    Rename anim.css to glitch.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import '../anim.css'
+	import '../glitch.css'
 </script>
 
 <div class="flex flex-col justify-center items-center h-screen bg-background">
 	<section>
 		<div class="text-center font-bios">
-			<span class="text-3xl text-gray-400"> Welcome to </span>
-			<span class="text-7xl text-accent drop-shadow-xl glitch layers" data-text="Liempo">
-				Liempo
-			</span>
+			<span class="text-3xl text-gray-400"> Hello world, I'm </span>
+			<span class="text-7xl text-accent drop-shadow-xl glitch layers" data-text="ALEC"> Alec </span>
 		</div>
 	</section>
 </div>
